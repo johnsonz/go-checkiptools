9e7723023f42648c15069d805c7936292adfc705
Johnson Zhang <zhangcs.qust@gmail.com>
2017-07-03 14:50:28 +0800

    support comment in main.json

--- main.go
+++ main.go
@@ -214,9 +214,29 @@ func main() {
 
 //Parse config file
 func parseConfig() {
-	conf, err := ioutil.ReadFile(configFileName)
+	data, err := ioutil.ReadFile(configFileName)
 	checkErr("read config file error: ", err, Error)
-	err = json.Unmarshal(conf, &config)
+	var lines []string
+	for _, line := range strings.Split(strings.Replace(string(data), "\r\n", "\n", -1), "\n") {
+		line = strings.TrimSpace(line)
+		if !strings.HasPrefix(line, "//") && line != "" {
+			lines = append(lines, line)
+		}
+	}
+
+	var b bytes.Buffer
+	for i, line := range lines {
+		if len(lines)-1 > i {
+			nextLine := lines[i+1]
+			if nextLine == "]" || nextLine == "]," || nextLine == "}" || nextLine == "}," {
+				if strings.HasSuffix(line, ",") {
+					line = strings.TrimSuffix(line, ",")
+				}
+			}
+		}
+		b.WriteString(line)
+	}
+	err = json.Unmarshal(b.Bytes(), &config)
 	checkErr("parse config file error: ", err, Error)
 }
 
